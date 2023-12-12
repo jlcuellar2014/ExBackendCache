36957d94d53f18db5987a43c9c226a0a772278d8
jlcuellar2014 <35740606+jlcuellar2014@users.noreply.github.com>
2023-12-12 18:51:20 +0100

    Examples for branch endpoints were added

---
+++ CacheInMemory/DTOs/Examples/BranchReadDTOExample.cs
@@ -0,0 +1,28 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+
+namespace CacheInMemory.DTOs.Examples
+{
+    public class BranchReadDTOExample : IExamplesProvider<List<BranchReadDTO>>
+    {
+        public List<BranchReadDTO> GetExamples()
+        {
+            return new List<BranchReadDTO> {
+                new BranchReadDTO
+                {
+                    BranchId = 1,
+                    Name = "Mercedes Benz",
+                    Description = "German Cars",
+                    CountryCode = "DE"
+
+                },
+                new BranchReadDTO
+                {
+                    BranchId = 2,
+                    Name = "Toyota",
+                    CountryCode = "JP",
+                    Description = "Japanise branch"
+                }
+            };
+        }
+    }
+}
---
+++ CacheInMemory/DTOs/Examples/BranchUpdateDTOExample.cs
@@ -0,0 +1,17 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+
+namespace CacheInMemory.DTOs.Examples
+{
+    public class BranchUpdateDTOExample : IExamplesProvider<BranchUpdateDTO>
+    {
+        public BranchUpdateDTO GetExamples()
+        {
+            return new BranchUpdateDTO
+            {
+                Name = "Toyota",
+                CountryCode = "JP",
+                Description = "Japanise branch"
+            };
+        }
+    }
+}
